aa4957835dbea6ecfab76e191170ee19eeccf3a6
박서영 <seokachuu@gmail.com>
2024-03-09 22:46:05 +0900

    Feat: 로그인 모달

--- src/app/(afterLogin)/home/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export default function Home() {
-  return <main>home폴더안에 있는 페이지</main>;
-}
--- src/app/(afterLogin)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function AfterLoginLayout({ children }) {
-  return (
-    <div>
-      애프터로그인 레이아웃
-      {children}
-    </div>
-  );
-}
