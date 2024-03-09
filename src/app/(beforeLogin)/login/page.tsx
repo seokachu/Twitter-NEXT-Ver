80e194875772c25e34785fd2f3ba20e468a7e771
박서영 <seokachuu@gmail.com>
2024-03-09 15:22:06 +0900

    Style: login css 완성

---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Login(): void {
+  redirect('/i/flow/login');
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-        Root layout
-        {children}
+        <div className={styles.container}>{children}</div>
       </body>
     </html>
   );
